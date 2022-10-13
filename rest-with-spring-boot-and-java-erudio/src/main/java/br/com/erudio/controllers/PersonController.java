224337fb09b1a95753d5b683b2c7cdc108db286d
guilherj <contato.guilhermenascimento@hotmail.com>
2022-10-13 11:58:52 -0300

    :up: chore: Add Flyway dependency in project and impl database migration using flyway.

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -20,7 +20,7 @@ import br.com.erudio.dto.v1.PersonDTO;
 import br.com.erudio.services.PersonService;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired
---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(2, 'Duque de Caxias - Rio de Janeiro - Brasil', 'Guilherme', 'Male', 'Castilho'),
+	(3, 'Duque de Caxias - Rio de Janeiro - Brasil', 'Lidiane', 'Female', 'Castilho'),
+	(4, 'Duque de Caxias - Rio de Janeiro - Brasil', 'Gabriel', 'Male', 'Castilho'),
+	(5, 'India', 'Mahata', 'Male', 'Gandhi'),
+	(7, 'India', 'Ophashi', 'Male', 'Ananda'),
+	(8, 'India', 'Rajhi', 'Male', 'Ananda'),
+	(9, 'India', 'Indira', 'Female', 'Ananda'),
+	(10, 'India', 'Maia', 'Female', 'Ananda'),
+	(12, 'Gothan City - US', 'Bruce', 'Male', 'Wayne'),
+	(15, 'Metropolis - US', 'Clark', 'Male', 'Kent'),
+	(16, 'Themishira', 'Diana', 'Female', 'Princess');
+
