98c14eebce281cf11a99302bfadad88d078733ac
guilherj <contato.guilhermenascimento@hotmail.com>
2022-10-25 15:53:16 -0300

    :white_check_mark: test: Adding unit test in create, update and delete

--- rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/PersonServiceTest.java
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/PersonServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.stereotype.Repository;
 
+import br.com.erudio.dto.v1.PersonDTO;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 import br.com.erudio.services.PersonService;
@@ -24,74 +25,121 @@ import br.com.erudio.unittests.mapper.mocks.MockPerson;
 @TestInstance(Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-	
+
 	MockPerson input;
-	
+
 	@InjectMocks
 	private PersonService service;
-	
+
 	@Mock
 	private PersonRepository repository;
-	
 
 	@BeforeEach
 	void setUpMocks() throws Exception {
-		
+
 		input = new MockPerson();
-		
+
 		MockitoAnnotations.openMocks(this);
 	}
-	
+
 	@Test
 	void testFindById() {
-		
-		Person person = input.mockEntity(1);
-		person.setId(1L);
-		
+
+		Person entity = input.mockEntity(1);
+		entity.setId(1L);
+
 		/*
-		 * Quando o repository.findById for chamado o mockito vai interceptar
-		 * a chamada e retornará um optional do mock de person criado acima. 
+		 * Quando o repository.findById for chamado o mockito vai interceptar a chamada
+		 * e retornará um optional do mock de person criado acima.
 		 * 
 		 * PS.: Necessário colocar o PersonRepository com a notação @Mock como feito
 		 * mais a cima.
 		 */
-		when(repository.findById(1L)).thenReturn(Optional.of(person));
-		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
 		var result = service.findById(1L);
 		assertNotNull(result);
 		assertNotNull(result.getKey());
 		assertNotNull(result.getLinks());
-		
+
 		assertNotNull(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
-		
+
 		assertEquals("Addres Test1", result.getAddress());
 		assertEquals("First Name Test1", result.getFirstName());
 		assertEquals("Last Name Test1", result.getLastName());
 		assertEquals("Female", result.getGender());
-		
-		
-		
+
 	}
 
 	@Test
 	void testFindAll() {
 		fail("Not yet implemented");
 	}
-	
 
 	@Test
 	void testCreate() {
-		fail("Not yet implemented");
+
+		Person entity = input.mockEntity(1);
+
+		Person persisted = entity;
+		persisted.setId(1L);
+
+		PersonDTO dto = input.mockDTO(1);
+		dto.setKey(1L);
+
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.create(dto);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		assertNotNull(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
 	}
 
 	@Test
 	void testUpdate() {
-		fail("Not yet implemented");
+		Person entity = input.mockEntity(1);
+		entity.setId(1L);
+		
+		Person persisted = entity;
+		persisted.setId(1L);
+
+		PersonDTO dto = input.mockDTO(1);
+		dto.setKey(1L);
+
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+
+		var result = service.update(dto);
+
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+
+		assertNotNull(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
 	}
 
 	@Test
 	void testDelete() {
-		fail("Not yet implemented");
+		Person entity = input.mockEntity(1);
+		entity.setId(1L);
+
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
+		service.delete(1L);
 	}
 
 }
